06cca6ca5b6eb5682fd361d04027efe2bdb080cb
Casey Duncan <casey.duncan@gmail.com>
2012-06-02 17:21:46 -0600

    add some comments

--- js/app/hello.js
+++ js/app/hello.js
@@ -1,8 +1,14 @@
 // Hello World Example Apres Controller
 
 define(['jquery'], function($) {
+  // A controller is a simple object that exposes some methods
+  // It is the base API for the view being served
   var controller = {};
 
+  // The ready() method will be called by apres when the
+  // page DOM is fully loaded, like $(document).ready() in jQuery
+  // Apres will also pass in any query parameters from the url
+  // but we do not need them for this simple example
   controller.ready = function() {
     document.title = 'Hello World!';
     $('body').prepend(
@@ -11,6 +17,8 @@ define(['jquery'], function($) {
     );
   }
 
+  // The controller object we return is accessible as
+  // apres.controller by other scripts and widgets in the view
   return controller;
 });
 
