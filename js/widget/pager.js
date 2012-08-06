ca3bb3aaf5210428080c17ce4b7d1fc0204c3b92
Casey Duncan <casey.duncan@gmail.com>
2012-08-06 01:50:13 -0600

    wip pager demo and basic skin

--- js/widget/pager.js
+++ js/widget/pager.js
@@ -4,20 +4,18 @@
 // Part of the Apres suite http://apres.github.com
 // Apres is released under the MIT license
 
-define(['jquery'], function($) {
+define(['apres', 'jquery'], function(apres, $) {
   //## Basic Pager Delegate
   var BasicPagerDelegate = function(elem, pages, current) {
     this.pages = pages;
-    this.current = current;
     this.$el = elem;
-    var self = this;
-    setTimeout(function() {self.$el.trigger('pagerCurrentPage', self)}, 0);
+    this.currentPage(current);
   }
   BasicPagerDelegate.prototype.currentPage = function(index) {
     if (typeof index !== 'undefined' && index !== this.current 
         && index >= 0 && index < this.pages) {
       this.current = index;
-      this.$el.trigger('pagerCurrentPage', this);
+      this.$el.trigger('pager-currentPage', this);
     }
     return {index: this.current};
   }
@@ -72,13 +70,88 @@ define(['jquery'], function($) {
       this.$el.trigger('pagerDelegate', this);
     }
   }
+  PagerWidget.prototype.events = {
+    'click .action-currentPage': function(evt) {
+      var page = evt.target.getAttribute('data-page');
+      if (page) {
+        this.delegate().currentPage(Number(page));
+        evt.preventDefault();
+      }
+    },
+    'click .action-previousPage': function() {
+      var delegate = this.delegate();
+      var index = delegate.currentPage().index - 1;
+      if (index >= 0) delegate.currentPage(index);
+    },
+    'click .action-nextPage': function() {
+      var delegate = this.delegate();
+      var index = delegate.currentPage().index + 1;
+      if (index < delegate.pageCount()) delegate.currentPage(index);
+    },
+  };
   PagerWidget.prototype.BasicPagerDelegate = BasicPagerDelegate;
-  PagerWidget.widgetParams = [
-    {pages: {type: 'int', descr: 'If used standalone, specifies the number of pages. ' 
-      + 'This value is ignored if a delegate widget is specified.'}},
-    {current: {type: 'int', default: 0, descr: 'Used to specify the current page index '
-      + 'when *pages* is also specified.'}},
-  ];
+  PagerWidget.widgetParams = {
+    pages: {type: 'int', descr: 'If used standalone, specifies the number of pages. ' 
+      + 'This value is ignored if a delegate widget is specified.'},
+    current: {type: 'int', default: 0, descr: 'Used to specify the current page index '
+      + 'when *pages* is also specified.'},
+  };
+
+  //## Define built-in pager skins
+  var skins = PagerWidget.skins = {};
+
+/*
+  skins.basic = function() {
+    this.innerHtml = function(widget) {
+      return brackets(
+        ['a class="previousPage"', '◀'],
+        brackets.repeat(widget.delegate().pageCount(), function(i) {
+          return ['a class="currentPage"', {'data-page': i}, i + 1]
+        }),
+        ['a class="nextPage"', '▶']
+      );
+    }
+  }
+*/
+
+  skins.basic = function(elem, widget) {
+    var pageLimit = 4;
+    var delegate = widget.delegate();
+    this.layout = function() {
+      var abs = Math.abs,
+          pages = this.pageCount = delegate.pageCount(),
+          current = this.pageIndex = delegate.currentPage().index,
+          html = '<a class="action-nextPage" href="#">▶</a>';
+      for (var i = pages - 1; i >= 0; i--) {
+        html = '<a class="action-currentPage page' + i + '" href="#" data-page="' + i + '">' + (i+1) + '</a>' + html;
+        if (i > pageLimit && (i <= current - 2 || i > current + 2)) {
+          html = '<span class="action-currentPage">&hellip;</span>' + html;
+          if (i > current) {
+            i = Math.max(pageLimit + 1, current + 3);
+          } else {
+            i = 1;
+          }
+        }
+      }
+      html = '<a class="action-previousPage" href="#">◀</a>' + html;
+      elem.html(html);
+      elem.find('.page' + current).addClass('current');
+    }
+    this.update = function() {
+      if (delegate.pageCount() > pageLimit) {
+        this.layout();
+      } else {
+        var current = delegate.currentPage().index;
+        elem.find('.action-currentPage.current').removeClass('current');
+        elem.find('.action-currentPage.page' + current).addClass('current');
+      }
+    }
+    this.events = {
+      'pager-pageschanged': 'layout',
+      'pager-currentPage': 'update',
+    }
+    apres.linkStyleSheet('/css/skin/pager.css');
+    elem.addClass('pager-basic-skin');
+  }
   return PagerWidget;
 });
-
